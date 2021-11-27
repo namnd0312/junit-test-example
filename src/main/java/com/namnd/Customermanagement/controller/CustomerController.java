5e2016fb03fe78ff7ce1e3d39216c77dd6586642
namnd0312 <namnd0312@gmail.com>
2021-11-27 20:45:49 +0700

    update

--- src/main/java/com/namnd/Customermanagement/controller/CustomerController.java
+++ src/main/java/com/namnd/Customermanagement/controller/CustomerController.java
@@ -1,15 +1,63 @@
 package com.namnd.Customermanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.namnd.Customermanagement.model.Customer;
+import com.namnd.Customermanagement.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
-	
-	@GetMapping("/")
-	String getInfo() {
-		return "Namnd";
-	}
+
+    @Autowired
+    private CustomerService customerService;
+
+
+    @PostMapping("/create")
+    ResponseEntity<Customer> createCustomer(Customer customer) {
+        Customer customerResult = this.customerService.createCustomer(customer);
+        return new ResponseEntity<>(customerResult, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    ResponseEntity<Customer> updateCustomer(Customer customer) {
+        Customer customerExist = this.customerService.findCustomerById(customer.getId());
+
+        if (customerExist == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Customer customerResult = this.customerService.createCustomer(customer);
+        return new ResponseEntity<>(customerResult, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    ResponseEntity<?> deleteCustomer(@PathVariable Long id) {
+        Customer customerExist = this.customerService.findCustomerById(id);
+
+        if (customerExist == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        this.customerService.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/getAll")
+    ResponseEntity<  List<Customer>> findAllCustomer() {
+        List<Customer> customerResult = this.customerService.findAll();
+        return new ResponseEntity<>(customerResult, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    ResponseEntity<Customer> findById(@PathVariable Long id) {
+        Customer customerResult = this.customerService.findCustomerById(id);
+        return new ResponseEntity<>(customerResult, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/namnd/Customermanagement/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/namnd/Customermanagement/service/impl/CustomerServiceImpl.java
@@ -1,42 +1,56 @@
 package com.namnd.Customermanagement.service.impl;
 
 import com.namnd.Customermanagement.model.Customer;
+import com.namnd.Customermanagement.repository.CustomerRepository;
 import com.namnd.Customermanagement.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @Override
     public Customer createCustomer(Customer customer) {
-        return null;
+        return customerRepository.save(customer);
     }
 
     @Override
     public Customer updateCustomer(Customer customer) {
+
+        if(customer.getId() != null){
+            return customerRepository.save(customer);
+        }
+
         return null;
     }
 
     @Override
     public Customer findCustomerByEmail(String email) {
-        return null;
+        return this.customerRepository.findByEmail(email);
     }
 
     @Override
     public Customer findCustomerById(Long id) {
-        return null;
+        Optional<Customer> customerResult = this.customerRepository.findById(id);
+        return customerResult.orElse(null);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        Optional<Customer> customerResult = this.customerRepository.findById(id);
+        if(customerResult.isPresent()){
+            this.customerRepository.deleteById(id);
+        }
     }
 
 	@Override
 	public List<Customer> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.customerRepository.findAll();
 	}
 }
